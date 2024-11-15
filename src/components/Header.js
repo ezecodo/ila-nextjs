5bccbe197dcaf5656571da64df61b3a11d689cc3
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2024-11-15 15:31:01 +0100

    hamburguer menu and responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,53 @@
-// src/components/Header.js
+"use client";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./Header.module.css";
 
-const Header = () => (
-  <header className={styles.header}>
-    <div className={styles.logoContainer}>
-      {/* Cambiar de <img> a <Image> */}
-      <Image
-        src="/ila-logo.png"
-        alt="ILA Logo"
-        width={50} // Ajusta el tamaño del logo según lo necesites
-        height={50} // Ajusta el tamaño del logo según lo necesites
-        className={styles.logo}
-      />
-      <span className={styles.tagline}>Das Lateinamerika-Magazin</span>
-    </div>
-    <nav className={styles.nav}>
-      <ul className={styles.menu}>
-        <li>
-          <Link href="/">Inicio</Link>
-        </li>
-        <li>
-          <Link href="/about">Sobre Nosotros</Link>
-        </li>
-        <li>
-          <Link href="/articles">Artículos</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contacto</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <Image
+          src="/ila-logo.png"
+          alt="ILA Logo"
+          width={50}
+          height={50}
+          className={styles.logo}
+        />
+        <span className={styles.tagline}>Das Lateinamerika-Magazin</span>
+      </div>
+      <button
+        className={styles.menuButton}
+        onClick={toggleMenu}
+        aria-label="Toggle menu"
+      >
+        ☰
+      </button>
+      <nav className={`${styles.nav} ${menuOpen ? styles.active : ""}`}>
+        <ul className={styles.menu}>
+          <li>
+            <Link href="/">Inicio</Link>
+          </li>
+          <li>
+            <Link href="/about">Sobre Nosotros</Link>
+          </li>
+          <li>
+            <Link href="/articles">Artículos</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contacto</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
