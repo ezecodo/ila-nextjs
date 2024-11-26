2dac178e898b742ecb7beff38a1af0c4442c39f9
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2024-11-26 17:32:16 +0100

    Corrección del endpoint GET /api/beitragstypen

--- src/app/api/beitragstypen/route.js
+++ src/app/api/beitragstypen/route.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET(req) {
+export async function GET() {
   try {
     // Obtener los Beitragstypen junto con sus subtipos
     const beitragstypen = await prisma.beitragstyp.findMany({
@@ -15,17 +15,22 @@ export async function GET(req) {
         JSON.stringify({ error: "No se encontraron tipos de artículos." }),
         {
           status: 404,
+          headers: { "Content-Type": "application/json" },
         }
       );
     }
 
-    return new Response(JSON.stringify(beitragstypen), { status: 200 });
+    return new Response(JSON.stringify(beitragstypen), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
     console.error("Error en el endpoint /api/beitragstypen:", error);
     return new Response(
       JSON.stringify({ error: "Error al cargar los tipos de artículo" }),
       {
         status: 500,
+        headers: { "Content-Type": "application/json" },
       }
     );
   } finally {
