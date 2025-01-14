5eefec634edc5532aaebd0d0dc768c9f5c3223c2
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2025-01-14 14:27:48 +0100

    next.config.ts modifief, image optimizer disable

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    unoptimized: true, // Desactiva el optimizador de imágenes
+  },
 };
 
 export default nextConfig;
