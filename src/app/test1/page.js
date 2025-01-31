020b103f9aed234425b2df51414d01e822d398be
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2025-01-31 12:08:03 +0100

    folder optimized

--- src/app/test1/page.js
+++
@@ -1,9 +0,0 @@
-import ArticleCardTest from "@/components/Articles/ArticleCardTest";
-
-export default function TestPage() {
-  return (
-    <div className="p-8 flex justify-center">
-      <ArticleCardTest />
-    </div>
-  );
-}
