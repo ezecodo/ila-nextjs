0b828785584c0078f6b2291dfefb016bac042f6c
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2025-10-20 20:06:58 +0200

    Abonnieren by DonationPopUP works  now the abonntment link

--- src/app/[locale]/components/DonationPopUp/DonationPopUp.js
+++ src/app/[locale]/components/DonationPopUp/DonationPopUp.js
@@ -49,7 +49,7 @@ export default function DonationPopup({ articleId }) {
             {t("donateButton")}
           </Link>
           <Link
-            href="/order/subscribe"
+            href="/order/abo"
             className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-800 transition"
           >
             {t("subscribeButton")}
