e586f9ff710e898d34da56c6996092c8619d7f71
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2026-01-08 11:30:19 +0100

    fix: suspense on aktuelles page added

--- src/app/[locale]/aktuell/aktuelles/page.tsx
+++ src/app/[locale]/aktuell/aktuelles/page.tsx
@@ -1,8 +1,19 @@
-// app/[locale]/aktuelles/page.js
 "use client";
 
+import { Suspense } from "react";
 import AktuellesList from "../../components/AktuellesList/AktuellesList";
+import IlaLoader from "../../components/IlaLoader/IlaLoader";
 
 export default function AktuellesPage() {
-  return <AktuellesList />;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex justify-center py-20">
+          <IlaLoader />
+        </div>
+      }
+    >
+      <AktuellesList />
+    </Suspense>
+  );
 }
