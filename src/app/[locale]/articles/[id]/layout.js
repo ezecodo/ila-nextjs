678a811e75d732393a8fc076747f3fb1c01b7321
Ezequiel Angeloni <e.zeangeloni@gmail.com>
2026-01-14 15:27:37 +0100

    Fix: Seo duplication on generatedMetadata files

--- src/app/[locale]/articles/[id]/layout.js
+++
@@ -1,9 +0,0 @@
-// app/[locale]/articles/[id]/layout.js
-
-import { generateMetadata } from "./generateMetadata";
-
-export { generateMetadata };
-
-export default function ArticleLayout({ children }) {
-  return children;
-}
